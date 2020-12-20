7b48028da9a1c93afbfc0c183b60227d387fc47e
Xiaosea <maysrp@gmail.com>
2020-12-20 21:38:06 +0800

    Update ssd1306.py

--- ssd1306.py
+++ ssd1306.py
@@ -99,9 +99,18 @@ class SSD1306:
 
     def text(self, string, x, y, col=1):
         self.framebuf.text(string, x, y, col)
-    def hline(self, x, y, w, col):
+
+    def line(self,x1, y1,x2,y2,col=1):
+        self.framebuf.line(x1, y1, x2, y2, col)
+
+    def hline(self, x, y,w, col=1):
         self.framebuf.hline(x, y, w, col)
 
+    def vline(x, y, h,  col=1):
+        self.framebuf.vline(x, y, h, col)
+    
+    def blit(buf,x,y,col=1):
+        self.framebuf.blit(buf, x, y,col)
 
 class SSD1306_I2C(SSD1306):
     def __init__(self, width, height, i2c, addr=0x3c, external_vcc=False):
